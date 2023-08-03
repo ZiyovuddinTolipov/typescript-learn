a08691f4c7bdd5e9934644cc3f0c5ac6da3b4601
tolipov <ziyovuddin.developer@gmail.com>
2023-08-03 13:05:33 +0500

    TypeScriptda  tiplarni ozgartirish va  birlashtirish!

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-"use strict";
-console.log("hello world");
-throw new Error("this is ErrorMessage");
-//# sourceMappingURL=app.js.map
+// TypeScriptda  tiplarni ozgartirish va  birlashtirish!
+// unknown type, union type , literal type, type aliases, required and optional properties operators in type
+// havfli usul
+var a = 20;
+var b = a;
+// havfsiz usul
+var c = 20.8298;
+var e = c;
+var f = c;
+// (c as number).toFixed(2);
+// (<number>c).toFixed(2);
+// union type 
+var g = 12;
+g = "sssssssssssssssssssss";
+g = true;
+// literal type
+var s;
+s = "sm";
+s = "md";
+s = "lg";
+var x = "OFf";
+x = true;
+var obj;
+obj = { name: "str" };
+obj = { age: 12 };
+obj = { name: "str", age: 23 };
--- app.ts
+++ app.ts
@@ -1,2 +1,44 @@
-console.log("hello world");
-throw new Error("this is ErrorMessage");
+// TypeScriptda  tiplarni ozgartirish va  birlashtirish!
+// unknown type, union type , literal type, type aliases, required and optional properties operators in type
+
+// havfli usul
+let a: any = 20;
+let b: number = a;
+
+// havfsiz usul
+let c: unknown = 20.8298;
+
+let e: number = <number>c;
+let f: number = c as number;
+
+// (c as number).toFixed(2);
+// (<number>c).toFixed(2);
+
+// union type 
+let g: string | number | boolean = 12;
+g = "sssssssssssssssssssss";
+g = true;
+
+// literal type
+let s: "sm" | "md" | "lg";
+s = "sm";
+s = "md";
+s = "lg";
+
+// type aliases
+
+type Sizes = "12" | "OFf" | true | 13 | number;
+
+let x: Sizes = "OFf";
+x = true;
+
+type OBJ = { name: string } | { age: number };
+
+let obj: OBJ;
+
+obj = { name: "str" };
+obj = { age: 12 };
+obj = { name: "str", age: 23 };
+
+type OBJ2 = { name: string } & { age: number };
+let obj2 : OBJ2 = { name: "str", age:12};
